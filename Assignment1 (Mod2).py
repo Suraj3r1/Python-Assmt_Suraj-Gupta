816d4529b5172640d998b87e0d27458376e3e139
Suraj3r1 <suraj3r1@gmail.com>
2025-06-16 13:38:28 +0530

    Add files via upload

---
+++ Assignment1 (Mod2).py
@@ -0,0 +1,23 @@
+#Shree Ganeshaya Namah
+
+a=input('Enter a :')
+b=input('Enter b :')
+a=float(a)
+b=float(b)
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+remainder=a%b
+print('Addition =',addition)
+print('Subtraction =',subtraction)
+print('Multiplication =',multiplication)
+print('Division =',division)
+print('Remainder =',remainder)
+
+
+
+
+
+
+
---
+++ Assignment2 (Mod2).py
@@ -0,0 +1,4 @@
+#Create a greeting for my son Vihaan Gupta
+First_name = input('Enter First name :')
+Last_name = input('Enter Last name :')
+print(f'Hey {First_name} {Last_name},Have a nice day!')
